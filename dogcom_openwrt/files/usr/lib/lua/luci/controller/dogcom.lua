d240fc0c9ad047628b4ae84387ac4e338a166bb5
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2019-04-17 14:37:51 +0800

    Update dogcom.lua

--- dogcom_openwrt/files/usr/lib/lua/luci/controller/dogcom.lua
+++ dogcom_openwrt/files/usr/lib/lua/luci/controller/dogcom.lua
@@ -13,7 +13,6 @@ function index()
 
 	entry({"admin", "services", "dogcom"},alias("admin", "services", "dogcom", "dogcom"),_("DROCM客户端")).dependent = true
 	entry({"admin", "services", "dogcom", "dogcom"}, cbi("dogcom/dogcom"),_("通用设置"),10).leaf = true
-	entry({"admin", "services", "dogcom", "dogcom_log"}, cbi("dogcom/dogcom_log"),_("运行日志"),20).leaf = true
 	entry({"admin", "services", "dogcom", "dogcom_proxy"}, cbi("dogcom/dogcom_proxy"),_("防代理设置"),30).leaf = true
 
 end
