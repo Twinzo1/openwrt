84a3d095205274491198619f5f71e711b822c894
Twinzo1 <44726207+Twinzo1@users.noreply.github.com>
2019-08-28 16:53:06 +0800

    Update mac.lua

--- luci-app-mac/luasrc/model/cbi/mac.lua
+++ luci-app-mac/luasrc/model/cbi/mac.lua
@@ -6,11 +6,11 @@ local m, s, o
 local fs = require "nixio.fs"
 local sys = require "luci.sys"
 
-m = Map("mac", translate("Mac地址"),
-	translate("克隆Wan口地址")
+m = Map("mac", translate("Mac address"),
+	translate("Clone Wan address")
 	..[[<br /><strong>]]
 	..[[<a href="https://github.com/Twinzo1/openwrt/tree/master/luci-app-mac" target="_blank">]]
-	..translate("项目地址")
+	..translate("Project address")
 	..[[</a>]]
 	..[[</strong><br />]])
 
@@ -22,28 +22,28 @@ s.anonymous = true
 os = s:option(Value,"mac",translate(""))
 os.template ="mac"
 
-enable=s:option(Flag,"enable",translate("定时修改Mac"))
+enable=s:option(Flag,"enable",translate("Timing change Mac"))
 enable.rmempty = false
 enable.default=0
 
-week=s:option(ListValue,"week",translate("周/天"))
-week:value(0,translate("每天"))
-week:value(1,translate("周一"))
-week:value(2,translate("周二"))
-week:value(3,translate("周三"))
-week:value(4,translate("周四"))
-week:value(5,translate("周五"))
-week:value(6,translate("周六"))
-week:value(7,translate("周日"))
+week=s:option(ListValue,"week",translate("Week/Day"))
+week:value(0,translate("Everyday"))
+week:value(1,translate("Monday"))
+week:value(2,translate("Tuesday"))
+week:value(3,translate("Wednesday"))
+week:value(4,translate("Thursday"))
+week:value(5,translate("Friday"))
+week:value(6,translate("Saturday"))
+week:value(7,translate("Sunday"))
 week.default=0
 week:depends({enable="1"})
 
-hour=s:option(Value,"hour",translate("小时"))
+hour=s:option(Value,"hour",translate("Hour"))
 hour.datatype = "range(0,23)"
 hour.default=0
 hour:depends({enable="1"})
 
-pass=s:option(Value,"minute",translate("分钟"))
+pass=s:option(Value,"minute",translate("Minute"))
 pass.datatype = "range(0,59)"
 pass.default=0
 pass:depends({enable="1"})
